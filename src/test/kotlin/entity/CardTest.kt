2d5dd4a0fd8e28c5d595a4f2f281958d9af2784b
Marvin Jütte <marvin.juette@udo.edu>
2023-04-22 12:07:23 +0200

    replace spaces with tabs

--- src/test/kotlin/entity/CardTest.kt
+++ src/test/kotlin/entity/CardTest.kt
@@ -9,28 +9,28 @@ import kotlin.test.assertEquals
  */
 class CardTest {
 
-   /**
-	* This test tests all possible [CardSuit] enum values for the current card to be returned
-	*
-	* @param cardSuit is a value of the [CardSuit] enum
-	*/
-   @EnumSource
-   @ParameterizedTest(name = "card.suit should equal to {0}")
-   fun getCardSuit(cardSuit: CardSuit) {
-	  val card = Card(cardSuit, CardValue.ACE)
-	  assertEquals(card.suit, cardSuit)
-   }
+	/**
+	 * This test tests all possible [CardSuit] enum values for the current card to be returned
+	 *
+	 * @param cardSuit is a value of the [CardSuit] enum
+	 */
+	@EnumSource
+	@ParameterizedTest(name = "card.suit should equal to {0}")
+	fun getCardSuit(cardSuit: CardSuit) {
+		val card = Card(cardSuit, CardValue.ACE)
+		assertEquals(card.suit, cardSuit)
+	}
 
 
-   /**
-	* This test tests all possible [CardValue] enum values for the current card to be returned
-	*
-	* @param cardValue is a value of the [CardValue] enum
-	*/
-   @EnumSource
-   @ParameterizedTest(name = "card.value should equal to {0}")
-   fun getCardValue(cardValue: CardValue) {
-	  val card = Card(CardSuit.CLUBS, cardValue)
-	  assertEquals(card.value, cardValue)
-   }
+	/**
+	 * This test tests all possible [CardValue] enum values for the current card to be returned
+	 *
+	 * @param cardValue is a value of the [CardValue] enum
+	 */
+	@EnumSource
+	@ParameterizedTest(name = "card.value should equal to {0}")
+	fun getCardValue(cardValue: CardValue) {
+		val card = Card(CardSuit.CLUBS, cardValue)
+		assertEquals(card.value, cardValue)
+	}
 }
