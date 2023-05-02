5c7bddeda0cfb32e5650446c5978f8886bf7f74b
Marvin <marvin.juette@udo.edu>
2023-05-02 07:25:54 +0200

    add missing score attribute

--- src/main/kotlin/entity/CardValue.kt
+++ src/main/kotlin/entity/CardValue.kt
@@ -10,20 +10,20 @@ import java.util.*
  * 2 < 3 < ... < 10 < Jack < Queen < King < Ace
  *
  */
-enum class CardValue {
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE,
-	TEN,
-	JACK,
-	QUEEN,
-	KING,
-	ACE,
+enum class CardValue(val score: Int) {
+	TWO(2),
+	THREE(3),
+	FOUR(4),
+	FIVE(5),
+	SIX(6),
+	SEVEN(7),
+	EIGHT(8),
+	NINE(9),
+	TEN(10),
+	JACK(10),
+	QUEEN(10),
+	KING(10),
+	ACE(11),
 	;
 
 	/**
@@ -58,8 +58,4 @@ enum class CardValue {
 		}
 
 	}
-
-
-
-
 }
