6b9ac18405a9feaac586208a949e7aa4c04c0d59
Marvin Jütte <marvin.juette@udo.edu>
2023-05-15 18:24:50 +0200

    button only executes action if keycode is space or enter

--- src/main/kotlin/extensions/ButtonExt.kt
+++ src/main/kotlin/extensions/ButtonExt.kt
@@ -2,11 +2,16 @@ package extensions
 
 import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.event.Event
+import tools.aqua.bgw.event.KeyCode
 
 /**
  * Extension function for [Button]s so that the same action is triggert on keyTyped and mouseClickedEvent
  */
 fun Button.onButtonClicked(action: (Event) -> Unit) {
 	this.apply { onMouseClicked = action }
-	this.apply { onKeyTyped = action }
+	this.apply { onKeyPressed = {
+		if (it.keyCode == KeyCode.SPACE || it.keyCode == KeyCode.ENTER) {
+			it.run(action)
+		}
+	} }
 }
